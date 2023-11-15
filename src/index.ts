33e0c2beb3fea19d9eaa93e017f7f1cdef34b8cf
Arttu Pennanen <arttu@pennanen.org>
2023-11-15 21:54:22 +0200

    chore: add initial server infrastructure

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import { startPublishingTestPings } from './server/graphql/graphql'
 import { startChessServer } from './server/server'
 import logger from './utils/logger'
 
 startChessServer()
-	.then(() => {
-		logger.info('server started')
-		startPublishingTestPings()
-	})
+	.then(() => logger.info('server started'))
 	.catch(logger.error)
