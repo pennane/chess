8e6bc53fbd155ca2f181662acce0fe880b4f2e00
Arttu Pennanen <arttu.pennanen@hoxhunt.com>
2026-05-29 13:26:20 +0300

    chore(front): delete obsolete duplicated chess files

--- packages/front/src/chess/chess.constants.ts
+++
@@ -1,8 +0,0 @@
-export const CHESS_BOARD_SIZE = 8
-
-export const BLACK = 'BLACK'
-export const WHITE = 'WHITE'
-export const LIGHT = 'LIGHT'
-export const DARK = 'DARK'
-
-export const CHESS_PIECE_ITEM_TYPE = 'CHESS_PIECE'
